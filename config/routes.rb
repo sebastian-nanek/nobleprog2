fce5e8ab29708d86233027aaf1f37fadeac8d690
SN <snanek@gmail.com>
2021-11-09 11:35:28 +0000

    Layout to include sign up/in links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'posts#index'
+
   get '/privacy', to: 'static_pages#privacy'
   get '/aktualny_czas', to: 'static_pages#current_time'
 end
