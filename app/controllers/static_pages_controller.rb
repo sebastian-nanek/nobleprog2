4b3fc594142b39414df8d979cf3919017e57b537
Sebastian Nanek <snanek@gmail.com>
2021-11-09 08:13:22 +0000

    Rubocop

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
-    # localhost:3000/privacy?lang=en
-    def privacy
-        if params[:lang] == 'pl'
-            render 'privacy_pl'
-        elsif params[:lang] == 'es'
-            render 'privacy_es'
-        end
+  # localhost:3000/privacy?lang=en
+  def privacy
+    if params[:lang] == 'pl'
+      render 'privacy_pl'
+    elsif params[:lang] == 'es'
+      render 'privacy_es'
     end
+  end
 
-    def current_time
-        @nighttime = (DateTime.now.hour > 16 || DateTime.now.hour < 7)
-    end
+  def current_time
+    @nighttime = (DateTime.now.hour > 16 || DateTime.now.hour < 7)
+  end
 end
