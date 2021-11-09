9814a9a29106217656f2e93ae74c4a2d29e5969e
Sebastian Nanek <snanek@gmail.com>
2021-11-09 08:50:35 +0000

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do 
+    user = Faker::Internet.user('username', 'email')
+    User.create!(
+        name: user['username'],
+        email: user['email']
+    )
+end
+
+25.times do 
+    Post.create!(
+        title: Faker::Lorem.question,
+        content: Faker::Lorem.paragraph(sentence_count: rand(5)),
+        user:  User.order("random()").first
+    )
+end
